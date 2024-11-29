cf3f1b34f96b2934c0c5ddabac4ad4f425b1aaf5
100477706 <100477706@alumnos.uc3m.es>
2024-11-29 10:13:26 +0100

    Scroll y Seguimiento de Paginas

--- src/script/menuPrincipalTransition.js
+++ src/script/menuPrincipalTransition.js
@@ -18,9 +18,48 @@ function menuTransition(){
     }
 }
 
-function updateActiveLinkOnScroll() {
-    
+const sections = document.querySelectorAll("section");
+
+/*Función que actualiza el menu al descender por la web con la barra*/
+function detectSection() {
+    const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i];
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.offsetHeight;
+      const menuLinks = document.querySelectorAll('.MenuPrincipal a');
+
+        for (let j = 0; j < menuLinks.length; j++) {
+            menuLinks[j].classList.remove('active');
+        }
+
+        if (scrollPosition >= sectionTop && scrollPosition <= sectionBottom) {
+            if (section.id === "Page1"){   
+                menuLinks[0].classList.add('active');
+            }
+
+            else if (section.id === "Page2"){
+                menuLinks[1].classList.add('active');
+            }
+
+            else if (section.id === "Page3"){
+                menuLinks[2].classList.add('active');
+            }
+
+            else if (section.id === "Page4"){
+                menuLinks[3].classList.add('active');
+            }
+
+            else if (section.id === "Page5"){  
+                menuLinks[4].classList.add('active');
+            }
+        console.log(`Estás viendo: ${section.id}`);
+        break;
+        }
+    }
 }
 
+window.addEventListener("scroll", detectSection);
+
 window.onload = menuTransition();
-window.onload = updateActiveLinkOnScroll();
