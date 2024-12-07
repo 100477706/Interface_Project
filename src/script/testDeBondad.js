a09c46e747d867fcd80def9bf2d7f27c955e5861
lucap <100496633@alumnos.uc3m.es>
2024-12-07 12:03:06 +0100

    Añadido carbón si eres niño malo

--- src/script/testDeBondad.js
+++ src/script/testDeBondad.js
@@ -1,15 +1,17 @@
 function testResults(){
     let q1 = Number(document.getElementById('generoso').value);
     let q2 = Number(document.getElementById('ayuda').value);
-    let q3 = - Number(document.getElementById('pelea').value);
+    let q3 = 4 - Number(document.getElementById('pelea').value);
     let q4 = Number(document.getElementById('obediencia').value);
     let result = (q1+q2+q3+q4)/4;
-    if (result<1){
+    document.getElementById("sendTestBondad").style.display = "none";
+    document.getElementById("testform").style.display = "none";
+    if (result<2){
         console.log("Has sido un niño malo");
+        document.getElementById("badkid").style.display = "block";
     }else{
         console.log("Has sido un niño bueno");
         document.getElementById("goodKid").style.display = "block";
-        document.getElementById("sendTestBondad").style.display = "none";
     }
 
 }
